8a6e410ca8f18d7c4d3db61d80299d3cbf94520a
leaker <admin@leelib.com>
2024-03-03 14:02:17 +0900

    add NewAxis

--- axis.go
+++ axis.go
@@ -6,3 +6,11 @@ func (a Axis) C() (col, row int) {
 	col, row = CellNameToCoordinates(string(a))
 	return
 }
+
+func (a Axis) String() string {
+	return string(a)
+}
+
+func NewAxis(col, row int) Axis {
+	return Axis(CoordinatesToCellName(col, row))
+}
