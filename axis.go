db6b1aeb430453674527f6d9ac03a4150335780d
leaker <admin@leelib.com>
2024-03-03 14:05:17 +0900

    add Axis:Add

--- axis.go
+++ axis.go
@@ -11,6 +11,21 @@ func (a Axis) String() string {
 	return string(a)
 }
 
+func (a Axis) Col() int {
+	col, _ := a.C()
+	return col
+}
+
+func (a Axis) Row() int {
+	_, row := a.C()
+	return row
+}
+
+func (a Axis) Add(col, row int) Axis {
+	c, r := a.C()
+	return NewAxis(c+col, r+row)
+}
+
 func NewAxis(col, row int) Axis {
 	return Axis(CoordinatesToCellName(col, row))
 }
